96a846c6d439093366e1b149f15e6b909a18b547
Damien <damien@leclubdesextras.com>
2018-07-06 17:07:08 +0200

    Fix unused import

--- src/components/appWrapper.js
+++ src/components/appWrapper.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import '../App.css';
 import {ProgressBar, Button} from 'react-materialize';
 import MovieList from '../containers/movie/movieList';
