6b16c2c48d21e06670d67f22e574be4bf92b1edb
Damien <damien@leclubdesextras.com>
2018-07-10 11:21:11 +0200

    movieService is now global and can be used without being build and configured every time

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,129 +2,154 @@ import env from '../environments/config'
 
 const movieServiceInitializer = function() {
 
-    let service = {
-        requestWrapper: async function(request, initialize) {
-            if(!request){
-                throw new Error('Missing request');
-            }
+    if(window 
+    && window.reactMovieDb
+    && window.ReactMovieDb.services 
+    && window.ReactMovieDb.services.movieService) {
+        
+    } else if(window) {
 
-            if(!this.ready && !initialize){
-                throw new Error('The API is not initialized, run getConfiguration first');
-            }
-
-            let response = {};
-            let json = {};
-
-            response = await fetch(request);
-
-            if(response) {
-                json = await response.json();
-            }
-
-            if(response.status >= 400 || response.status === 0) {
-                throw json;
-            }
-            
-            return json;
-        },
-        addImagePath(results) {
-
-            if(!results) {
-                return results;
-            }
-
-            if(!this.ready) {
-                return results;
-            }
-
-            const imagePathBuilder = (result, resultWithPath) => {
-                if(resultWithPath.backdrop_path) {
-                    resultWithPath.backdrop_path = `${this.configuration.images.secure_base_url}${this.configuration.images.backdrop_sizes[2]}${result.backdrop_path}`;
+        let service = {
+            requestWrapper: async function(request, initialize) {
+                if(!request){
+                    throw new Error('Missing request');
                 }
-                if(resultWithPath.logo_path) {
-                    resultWithPath.logo_path = `${this.configuration.images.secure_base_url}${this.configuration.images.logo_sizes[1]}${result.logo_path}`;
-                }
-                if(resultWithPath.poster_path) {
-                    resultWithPath.poster_path = `${this.configuration.images.secure_base_url}${this.configuration.images.poster_sizes[2]}${result.poster_path}`;
-                }
-                if(resultWithPath.profile_path) {
-                    resultWithPath.profile_path = `${this.configuration.images.secure_base_url}${this.configuration.images.profile_sizes[1]}${result.profile_path}`;
-                }
-                if(resultWithPath.still_path) {
-                    resultWithPath.still_path = `${this.configuration.images.secure_base_url}${this.configuration.images.still_sizes[2]}${result.still_path}`;
-                }
-            }
-
-            if(Array.isArray(results)) {
-                results = results.map(result => {
-                    let resultWithPath = {
-                        ...result
-                    }
-
-                    imagePathBuilder(result, resultWithPath);
     
-                    return {
-                        ...resultWithPath,
-                    }
-                })
-            } else {
-                let resultWithPath = {
-                    ...results
+                if(!this.ready && !initialize){
+                    throw new Error('The API is not initialized, run getConfiguration first');
                 }
+    
+                let response = {};
+                let json = {};
+    
+                response = await fetch(request);
+    
+                if(response) {
+                    json = await response.json();
+                }
+    
+                if(response.status >= 400 || response.status === 0) {
+                    throw json;
+                }
+                
+                return json;
+            },
+            addImagePath(results) {
+    
+                if(!results) {
+                    return results;
+                }
+    
+                if(!this.ready) {
+                    return results;
+                }
+    
+                const imagePathBuilder = (result, resultWithPath) => {
+                    if(resultWithPath.backdrop_path) {
+                        resultWithPath.backdrop_path = `${this.configuration.images.secure_base_url}${this.configuration.images.backdrop_sizes[2]}${result.backdrop_path}`;
+                    }
+                    if(resultWithPath.logo_path) {
+                        resultWithPath.logo_path = `${this.configuration.images.secure_base_url}${this.configuration.images.logo_sizes[1]}${result.logo_path}`;
+                    }
+                    if(resultWithPath.poster_path) {
+                        resultWithPath.poster_path = `${this.configuration.images.secure_base_url}${this.configuration.images.poster_sizes[2]}${result.poster_path}`;
+                    }
+                    if(resultWithPath.profile_path) {
+                        resultWithPath.profile_path = `${this.configuration.images.secure_base_url}${this.configuration.images.profile_sizes[1]}${result.profile_path}`;
+                    }
+                    if(resultWithPath.still_path) {
+                        resultWithPath.still_path = `${this.configuration.images.secure_base_url}${this.configuration.images.still_sizes[2]}${result.still_path}`;
+                    }
+                }
+    
+                if(Array.isArray(results)) {
+                    results = results.map(result => {
+                        let resultWithPath = {
+                            ...result
+                        }
+    
+                        imagePathBuilder(result, resultWithPath);
+        
+                        return {
+                            ...resultWithPath,
+                        }
+                    })
+                } else {
+                    let resultWithPath = {
+                        ...results
+                    }
+    
+                    imagePathBuilder(results, resultWithPath);
+    
+                    results = resultWithPath;
+                }
+    
+                return results;
+            },
+            getConfiguration: async function() {
+                const request = `https://api.themoviedb.org/3/configuration?${this.key}`
+    
+                this.configuration = await this.requestWrapper(request, true);
+                this.ready = true;
+                return this.configuration;
+            },
+            getMovieNowPlaying: async function(page) {
+                if(page && typeof page !== 'number') {
+                    throw new Error('page must be a number')
+                }
+    
+                if(!page || page <= 0) {
+                    page = 1;
+                }
+    
+                const request = `https://api.themoviedb.org/3/movie/now_playing?${this.key}&page=${page}`
+    
+                let response = await this.requestWrapper(request);
+                response.results = this.addImagePath(response.results);
+                return response;
+            },
+            getMovieDetails: async function(id) {
+                if(typeof id !== 'number') {
+                    throw new Error('id must be a number')
+                }
+    
+                const request = `https://api.themoviedb.org/3/movie/${id}?${this.key}&append_to_response=videos,credits`
+                
+                let response = await this.requestWrapper(request);
+                response = this.addImagePath(response);
+                return response;
+            },
+            reset: function() {
+                this.configuration = {};
+                this.ready = false;
+            },
+            configuration: {},
+            key: `api_key=${env.MOVIE_DB_API_KEY}`,
+            ready: false
+        }
 
-                imagePathBuilder(results, resultWithPath);
-
-                results = resultWithPath;
+        // Add the service to the global variable
+        // making it accessible easily from anywhere
+        window.reactMovieDb = window.reactMovieDb ? 
+        {
+            ...window.reactMovieDb,
+            services: {
+                movieService: service
             }
-
-            return results;
-        },
-        getConfiguration: async function() {
-            const request = `https://api.themoviedb.org/3/configuration?${this.key}`
-
-            this.configuration = await this.requestWrapper(request, true);
-            this.ready = true;
-            return this.configuration;
-        },
-        getMovieNowPlaying: async function(page) {
-            if(page && typeof page !== 'number') {
-                throw new Error('page must be a number')
+        } 
+        :
+        {
+            services: {
+                movieService: service
             }
-
-            if(!page || page <= 0) {
-                page = 1;
-            }
-
-            const request = `https://api.themoviedb.org/3/movie/now_playing?${this.key}&page=${page}`
-
-            let response = await this.requestWrapper(request);
-            response.results = this.addImagePath(response.results);
-            return response;
-        },
-        getMovieDetails: async function(id) {
-            if(typeof id !== 'number') {
-                throw new Error('id must be a number')
-            }
-
-            const request = `https://api.themoviedb.org/3/movie/${id}?${this.key}&append_to_response=videos,credits`
-            
-            let response = await this.requestWrapper(request);
-            response = this.addImagePath(response);
-            return response;
-        },
-        reset: function() {
-            this.configuration = {};
-            this.ready = false;
-        },
-        configuration: {},
-        key: `api_key=${env.MOVIE_DB_API_KEY}`,
-        ready: false
+        };
     }
 
     // initialize
     // load api configuration
     const serviceInitializer = async function(skip) {
-        if(skip) {
+        const service = window.reactMovieDb.services.movieService
+        if(skip || service.ready) {
             return service;
         }
         
